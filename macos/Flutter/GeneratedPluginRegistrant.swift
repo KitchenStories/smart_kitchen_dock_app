ab025ebc66ee6f8f15c52313e697ab8048f8e9da
Ruben Beltran del Rio <ruben@unlimited.pizza>
2023-09-08 14:44:13 +0200

    Commit dirty branch

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import flutter_smart_kitchen_dock
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  SmartKitchenDockPlugin.register(with: registry.registrar(forPlugin: "SmartKitchenDockPlugin"))
 }
